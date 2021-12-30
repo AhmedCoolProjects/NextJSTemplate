bb98f22587d1ec0ddd120238b6dceb96f3e48566
Ahmed Bargady <bargadyahmed2000@gmail.com>
2021-12-30 06:26:08 +0100

    tailwindcss + mui + dark and light mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
+import Layout from "@comp/utils/Layout";
+import Head from "next/head";
+import { AppProvider } from "../context/AppContext";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* favicon.ico */}
+        <link rel="icon" href="/favicon.ico" />
+        {/* apple-touch-icon.png */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
+      </Head>
+      <AppProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppProvider>
+    </>
+  );
 }
 
 export default MyApp;
